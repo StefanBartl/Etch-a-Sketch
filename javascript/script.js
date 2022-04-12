9f61300fb4cc9d65bf7a79a7f54fb0d636e36ee1
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-04-12 22:31:43 +0200

    First debugging

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
 // Variables
-const colorPicker = document.querySelector("#colorPicker");
-let colorPickerValue = document.querySelector("#colorPicker").value;
+const colorPicker = document.querySelector(".colorPicker");
+let colorPickerValue = document.querySelector(".colorPicker").value;
 
 let gridActiveColor = "#fff000"; 
 
-const colorBackground = document.querySelector("#colorBackground");
-let colorBackgroundValue = document.querySelector("#colorBackground").value;
+const colorBackground = document.querySelector(".colorBackground");
+let colorBackgroundValue = document.querySelector(".colorBackground").value;
 
 
 function Etch (num){
-const container = document.getElementById("container");
+const container = document.querySelector(".container");
 
 // Create the grid
 for (let x = 0; x <num; x++){
     let grid = document.createElement("div");
     container.appendChild(grid);
     grid.classList.add("grid");
-    grid.setAttribute("id", "firstGrids");
+    grid.setAttribute("id", "form__firstrids");
     let a = 960 / num;
     grid.style.height = a + "px";
     grid.style.width = a + "px";
@@ -46,38 +46,38 @@ for (let c = 0; c < gridArray.length; c++){
 }
 
 // Get the value from the form
-let number = parseInt(document.querySelector("#firstG").value);
+let number = parseInt(document.querySelector(".form__first").value);
 // Invoke grid with new values
 Etch(number);
 
 
 // New grid with new Resolution function
-const newGrid = document.querySelector("#btn");
+const newGrid = document.querySelector(".drawing__btn-res");
 newGrid.addEventListener('click', function(){
-    const container = document.getElementById("container");
+    const container = document.querySelector(".container");
     container.remove();
     const newContainer = document.createElement("div");
-    newContainer.setAttribute("id", "container");
-    const wrapper = document.getElementById("wrapper");
-    wrapper.appendChild(newContainer);
+    newContainer.classList.add("container");
+    const drawing__wrapper = document.querySelector(".drawing__wrapper");
+    drawing__wrapper.appendChild(newContainer);
 
-    let newNum = parseInt(document.querySelector("#firstG").value);
+    let newNum = parseInt(document.querySelector(".form__first").value);
     Etch(newNum);
 });
 
 // Remove drawing function
-const remove = document.getElementById("btnRemove");
+const remove = document.querySelector(".drawing__btn-rem");
 remove.addEventListener("click", function(){
     const gridArray = document.querySelectorAll(".gridX");
-    for (let c = 0; c < gridArray.length; c++){
-    gridArray[c].style.backgroundColor = colorBackgroundValue}});
+    for (let cell = 0; cell < gridArray.length; cell++){
+    gridArray[cell].style.backgroundColor = colorBackgroundValue}});
 
 // Change the color of the drawing function
 colorPicker.addEventListener('input', function(){
     colorPickerValue = colorPicker.value;
     const gridActiveArray = document.querySelectorAll(".gridActive");
-    for (let c = 0; c < gridActiveArray.length; c++){
-    gridActiveArray[c].style.backgroundColor = colorPickerValue;
+    for (let cell = 0; cell < gridActiveArray.length; cell++){
+    gridActiveArray[cell].style.backgroundColor = colorPickerValue;
     };
     gridActiveColor = colorPickerValue;
 })
@@ -93,11 +93,11 @@ colorBackground.addEventListener('input', function(){
 );
 
 //Double the Numbers in the forms function
-let firstN = document.querySelector("#firstG");
-let secoundN = document.querySelector("#secoundG");
+let firstN = document.querySelector(".form__first");
+let secoundN = document.querySelector(".form__second");
 firstN.addEventListener('input', function(){
-    document.querySelector("#secoundG").value = document.querySelector("#firstG").value;
+    document.querySelector(".form__second").value = document.querySelector(".form__first").value;
 });
 secoundN.addEventListener('input', function(){
-    document.querySelector("#firstG").value = document.querySelector("#secoundG").value;
+    document.querySelector(".form__first").value = document.querySelector(".form__second").value;
 });
