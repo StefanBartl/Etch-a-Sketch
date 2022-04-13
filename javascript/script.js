02ab7a8a5387916afc25d29b1bb00c005619ac85
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-04-13 09:51:44 +0200

    Update v1: Structure

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,32 @@
+
+
 // Variables
 const colorPicker = document.querySelector(".colorPicker");
 let colorPickerValue = document.querySelector(".colorPicker").value;
-
-let gridActiveColor = "#fff000"; 
-
 const colorBackground = document.querySelector(".colorBackground");
 let colorBackgroundValue = document.querySelector(".colorBackground").value;
 
+let gridActiveColor = colorPickerValue;
 
-function Etch (num){
+const remove = document.querySelector(".drawing__btn-rem");
 const container = document.querySelector(".container");
 
-// Create the grid
+let firstN = document.querySelector(".form__first");
+let secoundN = document.querySelector(".form__second");
+
+function NewResolution (num){
+
+// Create the first row
 for (let x = 0; x <num; x++){
     let grid = document.createElement("div");
-    container.appendChild(grid);
+    // Get actual container amd append new grid
+    actContainer = document.querySelector(".container");
+    actContainer.appendChild(grid);
     grid.classList.add("grid");
-    grid.setAttribute("id", "form__firstrids");
     let a = 960 / num;
     grid.style.height = a + "px";
     grid.style.width = a + "px";
-
+            // Create the columns
             for (let y = 0; y <num; y++){
                 let gridX = document.createElement("div");
                 grid.appendChild(gridX);
@@ -28,51 +34,49 @@ for (let x = 0; x <num; x++){
                 let a = 960 / num;
                 gridX.style.height = a + "px";
                 gridX.style.width = a + "px";
-                gridX.style.backgroundColor = colorBackgroundValue;
-            }
-}
+                gridX.style.backgroundColor = colorBackground.value;
+            };
+};
 
-// Add drawing function via EventListener
+//console.log(`NewResolution() end.`);
+};
+
+// ? Start application
+// Get the value from the form
+let number = parseInt(document.querySelector(".form__first").value);
+// Invoke grid with new values
+NewResolution(number);
+
+//#region Event-Listener
+
+// ! New grid with new Resolution 
+const newGrid = document.querySelector(".drawing__btn-res");
+newGrid.addEventListener('click', function(){
+    // Remove the old container
+    const container = document.querySelector(".container");
+    container.remove();
+    // Create new container & append it
+    const newContainer = document.createElement("div");
+    newContainer.classList.add("container");
+    const drawing__wrapper = document.querySelector(".drawing__wrapper");
+    drawing__wrapper.appendChild(newContainer);
+
+    // Invoke Etch() with new resolution
+    let newNum = parseInt(document.querySelector(".form__first").value);
+    NewResolution(newNum);
+});
+
+// ! Add drawing Event-Listener
 const gridArray = document.querySelectorAll(".gridX");
 for (let c = 0; c < gridArray.length; c++){
     gridArray[c].addEventListener('mouseenter', 
     function(){
         gridArray[c].style.backgroundColor = gridActiveColor;
         gridArray[c].classList.add("gridActive");
-    
     });
-}
+};
 
-}
-
-// Get the value from the form
-let number = parseInt(document.querySelector(".form__first").value);
-// Invoke grid with new values
-Etch(number);
-
-
-// New grid with new Resolution function
-const newGrid = document.querySelector(".drawing__btn-res");
-newGrid.addEventListener('click', function(){
-    const container = document.querySelector(".container");
-    container.remove();
-    const newContainer = document.createElement("div");
-    newContainer.classList.add("container");
-    const drawing__wrapper = document.querySelector(".drawing__wrapper");
-    drawing__wrapper.appendChild(newContainer);
-
-    let newNum = parseInt(document.querySelector(".form__first").value);
-    Etch(newNum);
-});
-
-// Remove drawing function
-const remove = document.querySelector(".drawing__btn-rem");
-remove.addEventListener("click", function(){
-    const gridArray = document.querySelectorAll(".gridX");
-    for (let cell = 0; cell < gridArray.length; cell++){
-    gridArray[cell].style.backgroundColor = colorBackgroundValue}});
-
-// Change the color of the drawing function
+// ! Change the color of the drawing Event-Listener
 colorPicker.addEventListener('input', function(){
     colorPickerValue = colorPicker.value;
     const gridActiveArray = document.querySelectorAll(".gridActive");
@@ -82,22 +86,32 @@ colorPicker.addEventListener('input', function(){
     gridActiveColor = colorPickerValue;
 })
 
-// Change the color of the background function
+// ! Remove drawing Event-Listener
+remove.addEventListener("click", function(){
+    const gridArray = document.querySelectorAll(".gridX");
+    for (let cell = 0; cell < gridArray.length; cell++){
+    gridArray[cell].style.backgroundColor = colorBackgroundValue}});
+
+
+// ! Change the color of the background function
 colorBackground.addEventListener('input', function(){
     colorBackgroundValue = colorBackground.value;
     const gridArray = document.querySelectorAll(".gridX");
     for (let c = 0; c < gridArray.length; c++){
         if (!gridArray[c].classList.contains("gridActive")) //Make sure, the Grid Element isn't drawed. Otherwise some would delete the drawing if changing background color. It's because we want only to change the background - except the backgrond of the drawing elements.
         gridArray[c].style.backgroundColor = colorBackgroundValue;
-    }}
+}}
 );
 
-//Double the Numbers in the forms function
-let firstN = document.querySelector(".form__first");
-let secoundN = document.querySelector(".form__second");
+
+//#endregion
+
+
+// ? Double the Numbers in the forms function
 firstN.addEventListener('input', function(){
     document.querySelector(".form__second").value = document.querySelector(".form__first").value;
 });
 secoundN.addEventListener('input', function(){
     document.querySelector(".form__first").value = document.querySelector(".form__second").value;
 });
+
